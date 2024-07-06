4b3b67a4ee53d02dddbe82e69c10bb3aa480b204
Walaff Fernandes <walaffonofre@icloud.com>
2024-07-06 13:27:56 -0300

    feat: update modelCase and typenameCase to use configCase

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,17 @@
 import { z } from 'zod'
 
 const configBoolean = z.enum(['true', 'false']).transform((arg) => JSON.parse(arg))
+const configCase = z.enum(['PascalCase', 'camelCase', 'snake_case']).default('PascalCase')
 
 export const configSchema = z.object({
 	relationModel: configBoolean.default('true').or(z.literal('default')),
 	modelSuffix: z.string().default('Model'),
-	modelCase: z.enum(['PascalCase', 'camelCase']).default('PascalCase'),
+	modelCase: configCase,
 	useDecimalJs: configBoolean.default('false'),
 	imports: z.string().optional(),
 	prismaJsonNullability: configBoolean.default('true'),
 	includeTypename: configBoolean.default('false').or(z.string()),
+	typenameCase: configCase,
 })
 
 export type Config = z.infer<typeof configSchema>
